42e7f02be55805150180fc9d96fc4d450821c002
104620 <79134175+104620@users.noreply.github.com>
2022-12-19 11:48:18 +0100

    Initial commit

---
+++ README.md
@@ -0,0 +1,3 @@
+# Task1-angular-ivy-enerw6
+
+[Edit on StackBlitz ⚡️](https://stackblitz.com/edit/angular-ivy-enerw6)
---
+++ src/app/app.component.ts
@@ -0,0 +1,32 @@
+import { Component, VERSION } from '@angular/core';
+import {FormControl, FormGroupDirective, NgForm, Validators} from '@angular/forms';
+import {ErrorStateMatcher} from '@angular/material/core';
+
+@Component({
+  selector: 'my-app',
+  templateUrl: './app.component.html',
+  styleUrls: [ './app.component.css' ]
+})
+export class AppComponent  {
+  name = 'Angular ' + VERSION.major;
+}
+
+/** Error when invalid control is dirty, touched, or submitted. */
+export class MyErrorStateMatcher implements ErrorStateMatcher {
+  isErrorState(control: FormControl | null, form: FormGroupDirective | NgForm | null): boolean {
+    const isSubmitted = form && form.submitted;
+    return !!(control && control.invalid && (control.dirty || control.touched || isSubmitted));
+  }
+}
+
+/** @title Input with a custom ErrorStateMatcher */
+@Component({
+  selector: 'input-error-state-matcher-example',
+  templateUrl: './app.component.html',
+  styleUrls: ['./app.component.css'],
+})
+export class InputErrorStateMatcherExample {
+  emailFormControl = new FormControl('', [Validators.required, Validators.email]);
+
+  matcher = new MyErrorStateMatcher();
+}
